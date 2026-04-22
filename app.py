d3cda8c8217210a16b8dca846e4704a81b040f8b
24250614-ai <24250614@turan-edu.kz>
2026-04-22 22:40:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="M.A.R.V.I.S", layout="centered")
 
-# ===== STYLE (дорого, но без перегруза) =====
+# ===== STYLE  =====
 st.markdown("""
 <style>
 .stApp {background: radial-gradient(circle at center, #05070f, #01020a); color: white;}
